ad80252c2294a9e92cdf10a0b155562c10e3b81c
Denys Lytviak <deh4er@gmail.com>
2019-06-02 12:01:54 +0200

    something new

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#005f5f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "A", "O", "E", "U", "1", "2", "3", "4" };
+static const char *tags[] = { "A", "O", "E", "U", "1" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "tabbed-surf",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
